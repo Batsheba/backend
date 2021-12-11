fa0ac1d6750765badac7e458671fdaf124a31261
Batsheba <betsaivette.ms@gmail.com>
2021-12-10 20:10:10 -0500

    guardar list

---
+++ sesion01/01-variable.py
@@ -0,0 +1,30 @@
+#variale num
+numero=20
+numero2=20.2
+
+nom="bat"
+apellido='mal'
+
+fecha=0
+
+html ='''<html>
+<p>
+</p>'''
+
+print ('print:python')
+
+print(type(nom))
+print(type(numero2))
+print(type(numero))
+
+soltera=False
+cold=True
+print(type(soltera))
+
+list=[1,2,3,4,'ya viene por ti',[1,2],True]
+
+curso={
+    'nombre':'backend',
+     'dificultad':'dificil',
+}
+print (curso['nombre'])
