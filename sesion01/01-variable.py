29c89ea6f292e63698fe856e5d35b3b321d18d44
Batsheba <betsaivette.ms@gmail.com>
2021-12-10 21:49:26 -0500

    operacion

--- sesion01/01-variable.py
+++ sesion01/01-variable.py
@@ -26,5 +26,48 @@ list=[1,2,3,4,'ya viene por ti',[1,2],True]
 curso={
     'nombre':'backend',
      'dificultad':'dificil',
+    'skills':[
+    {
+    'nombre':'backend1',
+     'nivel':'dificil1',
+     },
+     {
+        'nombre':'backend2',
+       'nivel':'dificil2',  
+     }
+     ]
 }
-print (curso['nombre'])
+print (list[2:-2])
+print (curso['skills'][1]['nivel'])
+
+
+personas=[{
+     'nombre':'Luisa',
+     'nacionalidad':'peruano',
+    'hobbies':[
+    {
+    'nombre':'volar drones',
+     'experiencia':'2 años',
+     },
+     {
+        'nombre':'Programar ',
+       'experiencia':'1 mes',  
+     }
+     ]
+},{
+     'nombre':'Luis',
+     'nacionalidad':'colombiano',
+    'hobbies':[
+    {
+    'nombre':'montar bici',
+     'experiencia':'4 años',
+     },
+     {
+        'nombre':'base de datos ',
+       'experiencia':'8 mes',  
+     }
+     ]
+}]
+print (personas[0]['nacionalidad'])
+print(personas[1]['hobbies'])
+print(personas[0]['hobbies'][1]['experiencia'])
