cf800ff2a2b4c8c67d7d0e8cb35fd2e45f0d7975
Batsheba <betsaivette.ms@gmail.com>
2021-12-11 21:52:12 -0500

    commit

---
+++ ejerciciosSemana01/reto01.py
@@ -0,0 +1,9 @@
+persona = {
+    'nombre': 'Daniela',
+    'edad': '15',
+}
+
+if(persona['nombre']=='Daniela' and persona['edad']<='17'):
+    print('si es más de la 6.pm ir a dormir o si no hacer la tarea')
+else:
+    print('No estas obligada hacer tarea')
