b24a7f6c9ce161f323cf55c9317bacfe58fb3456
Batsheba <betsaivette.ms@gmail.com>
2021-12-17 19:01:56 -0500

    flask

--- semana02/inicios-flask.py
+++ semana02/inicios-flask.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def inicio():
     return 'Bienvenido a mi API '
 
+@app.route('/bienvenido')
+def bienvenido():
+    return 'Bienvenido a la API de Bat'
+
+@app.route('/home')
+def bienvenido_home():
+    return 'Bienvenido al psicologo '
 
 if __name__=='__main__':
-        app.run(debug=True)
+        app.run(debug=True, port=3000)
